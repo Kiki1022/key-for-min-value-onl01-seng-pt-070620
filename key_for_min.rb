40ae95e5c2b7ab8da5cff08d622903b336705b7d
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-18 19:31:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-names_hash.collect do |key, value|
+def key_for_min_value(hash)
+hash.collect do |key, value|
 end
  key 
 end
