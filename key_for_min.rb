4ff03d413b642e5eab2f9708d788985db9335240
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-18 19:31:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-hash.collect do |key, value|
+  hash.collect do |key, value|
 end
  key 
 end
